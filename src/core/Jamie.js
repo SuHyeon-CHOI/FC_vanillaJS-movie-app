49b9e1d2b7c58652faff58620eb20866712803c1
SuHyeon-CHOI <tngus4869@gmail.com>
2024-06-02 18:01:54 +0900

    first git commit

---
+++ src/core/Jamie.js
@@ -0,0 +1,10 @@
+// Component
+export class Component {
+  constructor(payload = {}) {
+    const { tagName = "div" } = payload;
+    this.el = document.createElement(tagName);
+  }
+
+  render() {}
+  // ...
+}
