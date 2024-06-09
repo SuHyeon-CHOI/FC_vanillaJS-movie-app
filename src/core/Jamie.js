dc89980e8e4f98a7e52057b3181dccc2e612d560
SuHyeon-CHOI <tngus4869@gmail.com>
2024-06-09 21:51:50 +0900

    에러 발생... 해결 중

--- src/core/Jamie.js
+++ src/core/Jamie.js
@@ -15,14 +15,30 @@ export class Component {
 
 // Router
 function routeRender(routes) {
+  if (!location.hash) {
+    history.replaceState(null, "", "/#/");
+  }
+
   const routerView = document.querySelector("router-view");
   // http://localhost:1234/#/about?name=jamie
   // #/about?name=jamie
   const [hash, queryString = ""] = location.hash.split("?");
 
-  routes.find((route) => {
-    return new RegExp(`${route.path}/?$`).test("hash");
-  });
+  // a=123&b=456
+  // ['a=123', 'b=456']
+  // { a: '123, b: '456'}
+  const query = queryString.split("&").reduce((acc, cur) => {
+    const [key, value] = cur.split("=");
+    acc[key] = value;
+    return acc;
+  }, {});
+  history.replaceState(query, "");
+
+  const currentRoute = routes.find((route) =>
+    new RegExp(`${route.path}/?$`).test("hash")
+  );
+  routerView.innerHTML = "";
+  routerView.append(new currentRoute.component().el);
 }
 export function createRouter(routes) {
   return function () {
